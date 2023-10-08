689e1cebed83db92c2015d449789bd5a48baa84e
webbgamers <webbgamers@gmail.com>
2023-10-07 22:44:38 -0400

    add endpoints and models

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,66 @@ use model::*;
 use std::env;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer};
-use mongodb::{Client, bson::doc};
+use mongodb::{
+    bson::{doc, oid::ObjectId, Document},
+    Client,
+};
 
 const DB_NAME: &str = "volunte";
 
 #[post("/register")]
-async fn register(client: web::Data<Client>, form: web::Json<User>) -> HttpResponse {
+async fn register(client: web::Data<Client>, form: web::Json<Register>) -> HttpResponse {
     let collection = client.database(DB_NAME).collection("users");
     let result = collection.insert_one(form.into_inner(), None).await;
     match result {
-        Ok(result) => HttpResponse::Ok().json(UserId {
-            id: result.inserted_id.as_object_id().unwrap().to_string(),
+        Ok(result) => HttpResponse::Ok().json(RegisterResponse {
+            id: result.inserted_id.as_object_id().unwrap().to_hex(),
         }),
-        Err(err) => HttpResponse::InternalServerError().json(Error {
+        Err(err) => HttpResponse::InternalServerError().json(ServerError {
             error: err.to_string(),
         }),
     }
 }
 
-
-#[post("/get_data")]
-async fn get_data() -> HttpResponse {
-    HttpResponse::Ok().body("OK")
+#[get("/event")]
+async fn get_event(client: web::Data<Client>, form: web::Json<GetEvent>) -> HttpResponse {
+    let collection = client.database(DB_NAME).collection::<EventFromBSON>("events");
+    let result = collection
+        .find_one(doc! { "_id": ObjectId::parse_str(&form.id).unwrap()}, None)
+        .await;
+    match result {
+        Ok(Some(event)) => HttpResponse::Ok().json(event),
+        Ok(None) => HttpResponse::NotFound().json(ServerError {
+            error: format!("No event found with id {}", form.id),
+        }),
+        Err(err) => HttpResponse::InternalServerError().json(ServerError {
+            error: err.to_string(),
+        }),
+    }
 }
 
+#[get("/login")]
+async fn login(client: web::Data<Client>, form: web::Json<Login>) -> HttpResponse {
+    let collection = client.database(DB_NAME).collection::<UserFromBSON>("users");
+    let result = collection.find_one(doc! {"email": &form.email, "password": &form.password}, None).await;
+    match result {
+        Ok(Some(user)) => HttpResponse::Ok().json(user),
+        Ok(None) => HttpResponse::Forbidden().json(ServerError { error: "Invalid email or password".to_string()}),
+        Err(err) => HttpResponse::InternalServerError().json(ServerError { error: err.to_string()})
+    }
+}
 
+#[get("/user")]
+async fn get_user(client: web::Data<Client>, form: web::Json<GetUser>) -> HttpResponse {
+    let collection = client.database(DB_NAME).collection::<UserFromBSON>("users");
+    let result = collection.find_one(doc! {"_id": ObjectId::parse_str(&form.id).unwrap()}, None).await;
+    match result {
+        Ok(Some(user)) => HttpResponse::Ok().json(user),
+        Ok(None) => HttpResponse::NotFound().json(ServerError { error: format!("No user found with id {}", form.id)}),
+        Err(err) => HttpResponse::InternalServerError().json(ServerError { error: err.to_string()})
+    }
+
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -48,7 +83,9 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(mdb_client.clone()))
             .service(register)
-            .service(get_data)
+            .service(get_event)
+            .service(login)
+            .service(get_user)
     })
     .bind((
         "127.0.0.1",
--- src/model.rs
+++ src/model.rs
@@ -1,18 +1,146 @@
+use mongodb::bson::serde_helpers::{rfc3339_string_as_bson_datetime, hex_string_as_object_id, deserialize_hex_string_from_object_id, serialize_hex_string_as_object_id, deserialize_rfc3339_string_from_bson_datetime, serialize_rfc3339_string_as_bson_datetime};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
-pub struct User {
+pub struct Register {
     pub email: String,
     pub name: String,
     pub password: String,
 }
 
 #[derive(Deserialize, Serialize)]
-pub struct UserId {
+pub struct RegisterResponse {
     pub id: String,
 }
 
 #[derive(Deserialize, Serialize)]
-pub struct Error {
+pub struct ServerError {
     pub error: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct GetEvent {
+    pub id: String,
+}
+
+
+#[derive(Deserialize, Serialize)]
+pub struct UserFromBSON {
+    #[serde(rename(deserialize="_id"), deserialize_with="deserialize_hex_string_from_object_id")]
+    pub id: String,
+    pub email: String,
+    pub name: String,
+    pub password: String,
+}
+
+
+#[derive(Deserialize, Serialize)]
+pub struct UserFromJSON {
+    #[serde(rename(serialize="_id"), serialize_with="serialize_hex_string_as_object_id")]
+    pub id: String,
+    pub email: String,
+    pub name: String,
+    pub password: String,
+}
+
+
+#[derive(Deserialize, Serialize)]
+pub struct EventFromBSON {
+    #[serde(rename(deserialize="_id"), deserialize_with="deserialize_hex_string_from_object_id")]
+    pub id: String,
+    pub name: String,
+    pub address: String,
+    pub timeslots: Vec<TimeSlotFromBSON>,
+    pub owner: OwnerFromBSON,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct EventFromJSON {
+    #[serde(rename(serialize="_id"), serialize_with="serialize_hex_string_as_object_id")]
+    pub id: String,
+    pub name: String,
+    pub address: String,
+    pub timeslots: Vec<TimeSlotFromJSON>,
+    pub owner: OwnerFromJSON,
+}
+
+
+#[derive(Deserialize, Serialize)]
+pub struct TimeSlotFromBSON {
+    #[serde(deserialize_with = "deserialize_rfc3339_string_from_bson_datetime")]
+    pub start: String,
+    #[serde(deserialize_with = "deserialize_rfc3339_string_from_bson_datetime")]
+    pub end: String,
+    pub volunteers: Vec<VolunteerFromBSON>,
+    pub requests: Vec<RequestFromBSON>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct TimeSlotFromJSON {
+    #[serde(serialize_with="serialize_rfc3339_string_as_bson_datetime")]
+    pub start: String,
+    #[serde(serialize_with="serialize_rfc3339_string_as_bson_datetime")]
+    pub end: String,
+    pub volunteers: Vec<VolunteerFromJSON>,
+    pub requests: Vec<RequestFromJSON>,
+}
+
+
+#[derive(Deserialize, Serialize)]
+pub struct VolunteerFromBSON {
+    #[serde(deserialize_with="deserialize_hex_string_from_object_id")]
+    pub id: String,
+    pub name: String,
+    pub role: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct VolunteerFromJSON {
+    #[serde(serialize_with="serialize_hex_string_as_object_id")]
+    pub id: String,
+    pub name: String,
+    pub role: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct RequestFromBSON {
+    #[serde(deserialize_with="deserialize_hex_string_from_object_id")]
+    pub id: String,
+    pub name: String,
+    pub roles: Vec<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct RequestFromJSON {
+    #[serde(serialize_with="serialize_hex_string_as_object_id")]
+    pub id: String,
+    pub name: String,
+    pub roles: Vec<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct OwnerFromBSON {
+    #[serde(deserialize_with="deserialize_hex_string_from_object_id")]
+    pub id: String,
+    pub name: String,
+}
+
+
+#[derive(Deserialize, Serialize)]
+pub struct OwnerFromJSON {
+    #[serde(serialize_with="serialize_hex_string_as_object_id")]
+    pub id: String,
+    pub name: String,
+}
+
+
+#[derive(Deserialize)]
+pub struct Login {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Deserialize)]
+pub struct GetUser {
+    pub id: String,
 }
